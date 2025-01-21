975e6b513af74cb00787f27dd2ab35f079b933c2
AnnamdhasuDileep <annamdhasdileep@gmail.com>
2025-01-21 19:09:50 +0530

    first trial

---
+++ src/main/java/MyAutomation/MyAutomation/App.java
@@ -0,0 +1,10 @@
+package MyAutomation.MyAutomation;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
